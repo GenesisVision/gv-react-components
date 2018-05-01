c686392c7c78526fb8d318dba9a1b672b9b9085b
nkrivous <kolsln@list.ru>
2018-05-01 20:18:14 +0300

    Update configs

--- rollup.config.js
+++ rollup.config.js
@@ -2,24 +2,25 @@ import copy from "rollup-plugin-copy";
 import commonjs from "rollup-plugin-commonjs";
 import resolve from "rollup-plugin-node-resolve";
 import uglify from "rollup-plugin-uglify";
+import sourceMaps from "rollup-plugin-sourcemaps";
 import { minify } from "uglify-es";
-
 export default {
-  input: "./src/index.ts",
+  input: "./compiled/index.js",
   output: {
     file: "dist/js/index.js",
-    format: "umd",
+    format: "es",
     name: "nkrivous-common",
     sourcemap: true
   },
   plugins: [
-    copy({
-      "./media/fonts": "dist/fonts",
-      "./media/css": "dist/css",
-      "./media/img": "dist/img"
-    }),
+    // copy({
+    //   "./media/fonts": "dist/fonts",
+    //   "./media/css": "dist/css",
+    //   "./media/img": "dist/img"
+    // }),
     resolve(),
     commonjs(),
-    uglify({}, minify)
+    uglify({}, minify),
+    sourceMaps()
   ]
 };
