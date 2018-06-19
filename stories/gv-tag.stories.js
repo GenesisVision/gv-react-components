2a73420b035f6666f418c70e93dd9fb11fc932fc
nkrivous <kolsln@list.ru>
2018-06-19 11:02:02 +0300

    Show components info

--- stories/gv-tag.stories.js
+++ stories/gv-tag.stories.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
+import { withInfo } from "@storybook/addon-info";
 import { GVTag, GVTagList } from "../dist/index.js";
 
-storiesOf("Tag", module).add("Tag", () => <GVTag tag="some tag" />);
-storiesOf("Tag", module).add("Tags", () => (
-  <GVTagList tags={["some tag", "some tag 2"]} />
-));
+storiesOf("Tag", module).add("Tag", withInfo()(() => <GVTag tag="some tag" />));
+
+storiesOf("Tag", module).add(
+  "Tags",
+  withInfo()(() => <GVTagList tags={["some tag", "some tag 2"]} />)
+);
