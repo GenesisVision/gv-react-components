db0a0e9f35701711232c77deb129f16fd146bfa7
nkrivous <kolsln@list.ru>
2018-07-24 15:23:26 +0300

    Remove redundant code

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
-import add from "./math/math";
 import GVTag from "./gv-tags/gv-tag";
 import GVTagList from "./gv-tags/gv-tag-list";
 import GVButton from "./gv-button";
 
-export { add, GVTag, GVTagList, GVButton };
+export { GVTag, GVTagList, GVButton };
--- src/math/math.ts
+++
@@ -1,4 +0,0 @@
-//get sum of a and b
-const add = (a: number, b: number): number => a + b;
-
-export default add;
