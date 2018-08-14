9452af13b88c20ec0fd5b060e5beb745e255aae6
Nikolai Krivous <nkrivous@192-168-1-77.local>
2018-08-14 16:48:52 +0300

    Update styles for text button

--- src/gv-button/style.scss.d.ts
+++ src/gv-button/style.scss.d.ts
@@ -1,5 +1,6 @@
 export const gvBtn: string;
 export const gvBtnPrimary: string;
 export const gvBtnSecondary: string;
+export const gvBtnText: string;
 export const gvBtnContained: string;
 export const gvBtnOutlined: string;
