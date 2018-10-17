55ae968d79dd18c9c2d30fb3b28c6c63ef1d083d
nkrivous <kolsln@list.ru>
2018-10-17 15:45:56 +0300

    Update avatar storybook

--- stories/gv-program-avatar.stories.js
+++ stories/gv-program-avatar.stories.js
@@ -2,7 +2,7 @@ import { storiesOf } from "@storybook/react";
 import { withInfo } from "@storybook/addon-info";
 import React from "react";
 
-import { GVProgramAvatar } from "../dist/index.js";
+import { GVColors, GVProgramAvatar } from "../dist/index.js";
 
 storiesOf("Program Avatar", module).add(
   "Avatar with level",
@@ -21,6 +21,7 @@ storiesOf("Program Avatar", module).add(
           url="https://example.com/1.png"
           level={7}
           alt="John Smith"
+          color="#abc"
         />
         Default / Broken url
       </div>
@@ -33,15 +34,30 @@ storiesOf("Program Avatar", module).add(
   withInfo()(() => (
     <div>
       <div className="flex vertical-center padding">
-        <GVProgramAvatar level={3} size="small" alt="The small avatar" />
+        <GVProgramAvatar
+          level={3}
+          size="small"
+          alt="The small avatar"
+          color={GVColors.$positiveColor}
+        />
         The small avatar
       </div>
       <div className="flex vertical-center padding">
-        <GVProgramAvatar level={3} size="medium" alt="THe Medium Avatar" />
+        <GVProgramAvatar
+          level={3}
+          size="medium"
+          alt="THe Medium Avatar"
+          color={GVColors.$positiveColor}
+        />
         The medium avatar
       </div>
       <div className="flex vertical-center padding">
-        <GVProgramAvatar level={3} size="big" alt="The Big Avatar" />
+        <GVProgramAvatar
+          level={3}
+          size="big"
+          alt="The Big Avatar"
+          color={GVColors.$positiveColor}
+        />
         The big avatar
       </div>
     </div>
@@ -53,35 +69,66 @@ storiesOf("Program Avatar", module).add(
   withInfo()(() => (
     <div>
       <div className="flex vertical-center padding">
-        <GVProgramAvatar level={1} size="small" alt="Level" />
+        <GVProgramAvatar
+          level={1}
+          size="small"
+          alt="Level 1"
+          color={GVColors.$positiveColor}
+        />
         Level 1
       </div>
       <div className="flex vertical-center padding">
-        <GVProgramAvatar level={2} size="small" alt="Level" />
+        <GVProgramAvatar
+          level={2}
+          size="small"
+          alt="Level 2"
+          color={GVColors.$positiveColor}
+        />
         Level 2
       </div>
       <div className="flex vertical-center padding">
-        <GVProgramAvatar level={2} size="small" alt="Level" />
-        Level 2
-      </div>
-      <div className="flex vertical-center padding">
-        <GVProgramAvatar level={3} size="small" alt="Level" />
+        <GVProgramAvatar
+          level={3}
+          size="small"
+          alt="Level 3"
+          color={GVColors.$positiveColor}
+        />
         Level 3
       </div>
       <div className="flex vertical-center padding">
-        <GVProgramAvatar level={4} size="small" alt="Level" />
+        <GVProgramAvatar
+          level={4}
+          size="small"
+          alt="Level 4"
+          color={GVColors.$positiveColor}
+        />
         Level 4
       </div>
       <div className="flex vertical-center padding">
-        <GVProgramAvatar level={5} size="small" alt="Level" />
+        <GVProgramAvatar
+          level={5}
+          size="small"
+          alt="Level 5"
+          color={GVColors.$positiveColor}
+        />
         Level 5
       </div>
       <div className="flex vertical-center padding">
-        <GVProgramAvatar level={6} size="small" alt="Level" />
+        <GVProgramAvatar
+          level={6}
+          size="small"
+          alt="Level 6"
+          color={GVColors.$positiveColor}
+        />
         Level 6
       </div>
       <div className="flex vertical-center padding">
-        <GVProgramAvatar level={7} size="small" alt="Level" />
+        <GVProgramAvatar
+          level={7}
+          size="small"
+          alt="Level 7"
+          color={GVColors.$positiveColor}
+        />
         Level 7
       </div>
     </div>
