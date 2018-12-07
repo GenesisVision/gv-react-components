38f0fe818c348cf5b91d0c87c824e96008cd3a03
pavelzubov <pasha224@yandex.ru>
2018-12-07 15:29:24 +0300

    Add test for handlers to avatar level

--- src/gv-program-avatar/index.test.tsx
+++ src/gv-program-avatar/index.test.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { mount, shallow } from "enzyme";
 import GVProgramAvatar from ".";
+import style from "./style.scss";
 
 describe("GVProgramAvatar tests", () => {
   test("should render program avatar", () => {
@@ -30,5 +31,41 @@ describe("GVProgramAvatar tests", () => {
       <GVProgramAvatar url="example.com/image.jpg" alt="avatar" level={1} />
     );
     expect(programAvatar.find(`.${levelClassName}`).text()).toBe("1");
+  });
+
+  test("should call click event", () => {
+    const handleClick = jest.fn();
+    const programAvatar = shallow(
+      <GVProgramAvatar url="example.com/image.jpg" alt="avatar" level={1}  onClickLevel={handleClick}/>
+    );
+    programAvatar.find(`.${style.programAvatarLevel}`).simulate("click");
+    expect(handleClick).toBeCalled();
+  });
+
+  test("should call enter event", () => {
+    const handleEnter = jest.fn();
+    const programAvatar = shallow(
+      <GVProgramAvatar url="example.com/image.jpg" alt="avatar" level={1}  onMouseEnterLevel={handleEnter}/>
+    );
+    programAvatar.find(`.${style.programAvatarLevel}`).simulate("mouseEnter");
+    expect(handleEnter).toBeCalled();
+  });
+
+  test("should call over event", () => {
+    const handleOver = jest.fn();
+    const programAvatar = shallow(
+      <GVProgramAvatar url="example.com/image.jpg" alt="avatar" level={1}  onMouseOverLevel={handleOver}/>
+    );
+    programAvatar.find(`.${style.programAvatarLevel}`).simulate("mouseOver");
+    expect(handleOver).toBeCalled();
+  });
+
+  test("should call leave event", () => {
+    const handleLeave = jest.fn();
+    const programAvatar = shallow(
+      <GVProgramAvatar url="example.com/image.jpg" alt="avatar" level={1}  onMouseLeaveLevel={handleLeave}/>
+    );
+    programAvatar.find(`.${style.programAvatarLevel}`).simulate("mouseLeave");
+    expect(handleLeave).toBeCalled();
   });
 });
